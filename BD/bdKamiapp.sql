fb3d1f8f68180a631861b990219069bcb31d6a27
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-06-16 12:47:07 -0500

    Actualizar tabla DireccionesEnvio en bdKamiapp.sql

--- BD/bdKamiapp.sql
+++ BD/bdKamiapp.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS Usuarios (
     idUsuario INT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     apellido VARCHAR(255) NOT NULL,
-    cedula VARCHAR(20) NOT NULL,
+    cedula VARCHAR(20) NOT NULL ,
     direccion VARCHAR(255) NOT NULL,
     telefono VARCHAR(20) NOT NULL,
     correo VARCHAR(255) NOT NULL,
